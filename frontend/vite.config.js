94caca105fb2db5f01ade0aadbc5d1ad40d030aa
anis <anis.chandoul@gmail.com>
2025-02-18 21:16:10 -0700

    rebuild

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ata/',
-  
+  base: '/ata/', // MUST be correct, with leading and trailing slashes
 });
