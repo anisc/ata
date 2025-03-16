0851024b20d28547e0fa302b5a1aca7822c783d8
anis <anis.chandoul@gmail.com>
2025-03-16 17:12:36 -0600

    env prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,31 @@
-import { defineConfig } from 'vite';
+// frontend/vite.config.js
+import { defineConfig, loadEnv } from 'vite'; // Import loadEnv
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5174, // Make sure this matches the port you're using
-    open: true, // Automatically open the browser
-  },
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    base: '/ata/', // Keep this for GitHub Pages
+
+    // Not needed anymore, Vite handles this automatically with .env files
+    // define: {
+    //   'process.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
+    // },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          }
+        }
+      }
+    }
+  }
 });
