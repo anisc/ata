db25fb666425b59f2e1855e1693791f14a9ba0d8
anis <anis.chandoul@gmail.com>
2025-03-16 17:17:26 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,14 @@
 // frontend/vite.config.js
-import { defineConfig, loadEnv } from 'vite'; // Import loadEnv
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
-    base: '/ata/', // Keep this for GitHub Pages
-
-    // Not needed anymore, Vite handles this automatically with .env files
-    // define: {
-    //   'process.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
-    // },
+    base: '/', // Change this to '/' for VPS deployment
     build: {
       rollupOptions: {
         output: {
